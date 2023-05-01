30029bb83a07f330d13c32234f26dac8408c1d98
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-05-01 12:44:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width = width;
+		this._height=height;
+	}
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this._height;
+	}
+	getArea(){
+		// let Area = width() * height();
+		let Area = this._width * this._height;
+		return Area;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	getPerimeter(){
+		let perimeter = 4 * this._width;
+		return perimeter;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+
